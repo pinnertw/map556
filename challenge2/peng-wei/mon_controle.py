6e93d78fa8b54eaee5d993b483bda3d685165494
pinnertw <wayne567tw@yahoo.com.tw>
2020-12-03 15:09:18 +0100

    [DEV] 60 coeffs, 130.4

--- challenge2/peng-wei/mon_controle.py
+++ challenge2/peng-wei/mon_controle.py
@@ -1,16 +1,11 @@
 import numpy as np
-m=np.load("umat.npy")
-db1=np.load("db1.npy")
-db2=np.load("db2.npy")
-ub1=np.load("ub1.npy")
-ub2=np.load("ub2.npy")
+coeffs = np.load("css.npy")
+seconds = np.arange(11)
+pos = np.zeros((11, 2))
+pos[:, 0] = seconds * 10
+pos[:, 1] = seconds * 20 - 2 * seconds**2
 def main(seconde, position):
-    s=int(seconde)
-    x,y=position
-    force=1.59*(np.array([10*s,20*s-2*s**2])-1.0125*position)
-    if t==0:
-        return m[0][0][0]+force
-    else:
-        i1=min(max(int((x1-db1[t-1])/(ub1[t-1]-db1[t-1])*2*t),0),2*t-1)
-        i2=min(max(int((x2-db2[t-1])/(ub2[t-1]-db2[t-1])*2*t),0),2*t-1)
-        return mat[t][i1][i2]+force
+    t = int(seconde)
+    s = t
+    #print(seconde, position, pos[t])
+    return coeffs[s, :2]*pos[t]-coeffs[s, 2:4]*position + coeffs[s, 4:]
