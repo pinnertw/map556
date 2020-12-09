eed15e3a34e160ae54ed1b8446a016cfb78a90d5
pinnertw <wayne567tw@yahoo.com.tw>
2020-12-09 17:04:53 +0100

    [DEV] Last. Cost at 114

--- challenge2/peng-wei/mon_controle.py
+++ challenge2/peng-wei/mon_controle.py
@@ -1,16 +1,13 @@
 import numpy as np
-coeffs = np.load("c_720.npy")                    #coeffs : the coefficients for all the members in the equation
-mean_states = np.load("mean_states_720.npy")     #mean_states : the average position at t=0 to t=9
-quantiles = np.load("quantiles_720.npy")         #the quantiles for the axes after being rotated from y=-x+h(t) to axe x
-seconds = np.arange(11)
-pos = np.zeros((11, 2))                      #pos : the position where the birds should be at different t.
-pos[:, 0] = seconds * 10
-pos[:, 1] = seconds * 20 - 2 * seconds**2
+coeffs = np.load("c_60.npy")                    #coeffs : the coefficients for all the members in the equation
+pos = np.load("mean_states_1440.npy")     #pos : the average position at t=0 to t=9
 sqr2 = 1/np.sqrt(2)
 rot = np.array([[sqr2, -sqr2],
                [sqr2, sqr2]])                #rot : the rotation matrix to rotate 45 degrees counter-clockwise
+rot2 = np.array([[sqr2, sqr2], [-sqr2, sqr2]]) #rot2 : rotation 45 degrees clockwise
 def main(seconde, position):
     t = int(seconde)
-    a, _ = rot.dot(position - mean_states[t])
-    i = np.searchsorted(quantiles[:, t],a, side="left")
-    return coeffs[t, i, :2]*pos[t]-coeffs[t, i, 2:4]*position + coeffs[t, i, 4:]
+    x1, x2 = np.matmul(rot, position - pos[t])
+    u1 = np.array([x1*(x1>0), x2*(x2>0)])
+    u2 = np.array([x1*(x1<0), x2*(x2<0)])
+    return np.matmul(rot2, coeffs[t,:2]*u1 + coeffs[t,2:4]*u2 + coeffs[t,4:6])
